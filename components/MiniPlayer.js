0f23a76d29b26f4d9b5bfb31ed48c3367af6e92e
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-05-14 00:43:33 +0530

    player ui

--- components/MiniPlayer.js
+++ components/MiniPlayer.js
@@ -9,25 +9,30 @@ const MiniPlayer = () => {
         <>
             <View style={styles.box}>
                <View style={styles.playerView}>
+                   <View style={styles.trackInfo} >
                    <Text style={{color : 'white'}} >Song Name</Text>
-                   <TouchableOpacity>
-                   <Image 
-                       source={require("../assets/previous.png")} 
-                        style={styles.playerIcons}
-                       />
-                   </TouchableOpacity>
-                   <TouchableOpacity>
-                       <Image 
-                       source={require("../assets/play-button.png")} 
-                        style={[styles.playerIcons, {width : 30, height : 30}]}
-                       />
-                   </TouchableOpacity>
-                   <TouchableOpacity>
-                   <Image 
-                       source={require("../assets/next.png")} 
-                        style={styles.playerIcons}
-                       />
-                   </TouchableOpacity>
+                   </View>
+
+                   <View style={styles.playerControls} >
+                        <TouchableOpacity>
+                        <Image 
+                            source={require("../assets/previous.png")} 
+                                style={styles.playerIcons}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Image 
+                            source={require("../assets/play-button.png")} 
+                                style={[styles.playerIcons]}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                        <Image 
+                            source={require("../assets/next.png")} 
+                                style={styles.playerIcons}
+                            />
+                        </TouchableOpacity>
+                   </View>
                </View>
             </View>
         </>
@@ -46,15 +51,26 @@ const styles = StyleSheet.create({
         bottom: 43,
         paddingVertical : 10,
         backgroundColor : '#212326',
-         marginBottom : 2
+         marginBottom : 1
     },
     playerView : {
         flex : 1,
         flexDirection : 'row',
-        
+        justifyContent : 'space-between'
+    },
+    trackInfo : {
+        marginStart : 10,
+        alignSelf : 'center'
+    },
+    playerControls : {
+        flexDirection : 'row',
+          marginEnd : 20,
+          alignSelf : 'center'
+      
     },
     playerIcons : {width : 25, 
         height : 25,
-         marginHorizontal : 10
-        }
+        marginHorizontal : 7
+        },
+    
 })
