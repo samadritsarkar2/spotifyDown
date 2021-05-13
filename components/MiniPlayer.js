9c232a351d556f051c11e12651e446973426db34
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-05-13 20:33:35 +0530

    miniplayer size

--- components/MiniPlayer.js
+++ components/MiniPlayer.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native"
-import { useNavigation } from "@react-navigation/native"
+import { useNavigation } from "@react-navigation/native";
+
 
 const MiniPlayer = () => {
    
     return (
         <>
             <View style={styles.box}>
-               <View>
-                   <Text>Song Name</Text>
+               <View style={styles.playerView}>
+                   <Text style={{color : 'white'}} >Song Name</Text>
                    <TouchableOpacity>
                        <Text>Play/Pause</Text>
                    </TouchableOpacity>
@@ -25,13 +26,19 @@ export default MiniPlayer;
 
 const styles = StyleSheet.create({
     box : {
-        
         position: 'absolute',
         width: '100%',
-        height: 40,
-        alignItems: 'center',
+        height: 47,
+      
         justifyContent: 'center',
         bottom: 43,
         paddingVertical : 10,
+        backgroundColor : '#212326',
+         marginBottom : 2
+    },
+    playerView : {
+        flex : 1,
+        flexDirection : 'row',
+        
     }
 })
---
+++ service.js
@@ -0,0 +1,5 @@
+// import TrackPlayer from "react-native-track-player"
+// module.exports = async function () {
+//   TrackPlayer.addEventListener("remote-play", () => TrackPlayer.play())
+//   TrackPlayer.addEventListener("remote-pause", () => TrackPlayer.pause())
+// }
