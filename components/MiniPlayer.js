23717b864ead8cf118983c4e6c26dc44c1f39ee1
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-05-15 23:00:51 +0530

    player

--- components/MiniPlayer.js
+++ components/MiniPlayer.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native"
-
+import { useNavigation } from "@react-navigation/native";
 
 
 const MiniPlayer = () => {
