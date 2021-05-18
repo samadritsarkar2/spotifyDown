804ae0fd162b07bd5a358d2c28301a69628f0f2b
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-05-18 19:22:54 +0530

    track player basic setup

--- index.js
+++ index.js
@@ -5,5 +5,10 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import Routes from './Routes';
+import TrackPlayer from "react-native-track-player";
+
 
 AppRegistry.registerComponent(appName, () => Routes);
+
+
+TrackPlayer.registerPlaybackService(()=> require("./service"));
