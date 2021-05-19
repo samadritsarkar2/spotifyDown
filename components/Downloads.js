55c6840780b2ae16dbc762cdbd219505e4f28bca
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-05-19 12:01:26 +0530

    downloads page

---
+++ components/Downloads.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import {Text, View} from "react-native";
+
+const Downloads = () => {
+
+    return (
+        <>
+            <View>
+                <Text>Downloads Page</Text>
+            </View>
+        </>
+    )
+}
+
+export default Downloads;
