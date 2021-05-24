d1eefc9cef4e83400d17fa2c36e23f2d96e0cec0
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-05-24 12:20:48 +0530

    library UI

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,11 +1,5 @@
 import React, {useState} from 'react'
-import { Button, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useFocusEffect } from "@react-navigation/native";
-import allActions from "../redux/actions/index";
-import Spinner from "react-native-spinkit";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import SavedPlaylists from './SavedPlaylists';
 
 
@@ -14,21 +8,29 @@ const Library = ({navigation}) => {
 
     return(
         <>
-        <View style={{flex : 1, backgroundColor : '#181818'}}>
+        <View style={{flex : 1, backgroundColor : '#181818', paddingHorizontal : 10}}>
             <View style={styles.header}>
                 <TouchableOpacity
                 onPress={() => navigation.navigate("Downloads")}
                 >
                 <Text style={styles.heading}>Your Library</Text>
                 </TouchableOpacity>
-                <View style={styles.actions}>
-                    <View style={{}} ><Text style={styles.buttons}>Saved Playlists  </Text></View>
-                </View>
             </View>
+                <View style={styles.actions}>
+                    <ScrollView
+                   
+                    alwaysBounceVertical={true}
+                    >
+                            <View style={{}} ><Text style={styles.buttons}>Saved Playlists  </Text></View>
+                            
+                    </ScrollView>
+                
+                </View>
+           
 
-            <SavedPlaylists />
+
         
-        </View>
+            </View>
         </>
     )
 }
@@ -40,13 +42,7 @@ export default Library;
 const styles = StyleSheet.create({
     header : {
         flex : 0.3,
-        marginTop : 40,
-        marginHorizontal : 20
-    },
-    library : {
-        flex : 0.7,
-       
-        marginHorizontal : 10
+        marginTop : '5%'
     },
     heading : {
         fontSize : 50,
@@ -54,24 +50,15 @@ const styles = StyleSheet.create({
         fontFamily : 'Gotham'
     },
     actions : {
-        flex : 0.7,
+        flex : 1,
         flexDirection : 'row',
         marginTop : 0,
         justifyContent : 'flex-start',
-        alignItems : 'flex-end'
+        
     },
     buttons : {
         fontSize : 25, 
         color : 'lightgray',
-    },
-    listBox : {
-        margin : 10,
-        
-    },
-    listText : {
-        fontSize : 25,
-        color : 'white',
-        fontWeight : '200'
     },
     submit : {
         justifyContent : "center",
--- src/navigation/LibraryStack.js
+++ src/navigation/LibraryStack.js
@@ -3,6 +3,7 @@ import {createStackNavigator} from "@react-navigation/stack"
 import { NavigationContainer } from '@react-navigation/native';
 import Library from "../components/Library";
 import Downloads from "../components/Downloads"
+import SavedPlaylists from "../components/SavedPlaylists";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ const LibraryStack = () => {
             headerMode={"none"}
             >
                 <Stack.Screen component={Library} name="Library" />
+                <Stack.Screen component={SavedPlaylists} name="SavedPlaylists" />
                 <Stack.Screen component={Downloads} name="Downloads" />
             </Stack.Navigator>
   
