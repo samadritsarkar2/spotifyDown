98150023b1358664c4b686eddf30db1ab7ffee0a
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-06-11 20:22:48 +0530

    Supoort the devs page

--- src/components/Donation.js
+++ src/components/Donation.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 const Donations = () => {
   return (
@@ -12,10 +19,19 @@ const Donations = () => {
             Spotify Downloader is Ad-free and to support us, you can donate any
             amount or from the options below.
           </Text>
-          <Text style={styles.subHeading}>
-            More updates and features are on its way. You are awesome 🎉
+          <Text style={[styles.subHeading]}>
+            More updates and features are on its way.{'   '}Thank you for your
+            support! You are awesome 🎉
           </Text>
         </View>
+        <ScrollView style={styles.scroller}>
+          <TouchableOpacity>
+            <View style={styles.optionWrapper}>
+              <Text style={styles.optionText}>Tea</Text>
+              <Text style={styles.optionSubText}>Rs. 10</Text>
+            </View>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
     </>
   );
@@ -25,7 +41,7 @@ export default Donations;
 
 const styles = StyleSheet.create({
   header: {
-    flex: 0.3,
+    flex: 0.4,
     marginHorizontal: '1%',
   },
   heading: {
@@ -43,5 +59,29 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginTop: 20,
     textAlign: 'center',
+  },
+  scroller: {
+    flex: 0.7,
+    marginTop: 15,
+  },
+  optionWrapper: {
+    flex: 1,
+    margin: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    flexDirection: 'column',
+    alignItems: 'baseline',
+    backgroundColor: '#111111',
+    borderRadius: 10,
+  },
+  optionText: {
+    color: 'white',
+    fontSize: 18,
+    fontFamily: 'GothamMedium',
+  },
+  optionSubText: {
+    color: '#6C7A89',
+    fontSize: 12,
+    fontFamily: 'GothamMedium',
   },
 });
