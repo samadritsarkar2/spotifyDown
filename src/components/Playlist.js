95b8f4c6f1e612796b03cc3f5f9fc06193833c0b
Samadrit Sarkar <samadrit.sarkar2@gmail.com>
2021-08-09 00:18:45 +0530

    Trying new cloud functions api

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -21,7 +21,7 @@ import Spinner from 'react-native-spinkit';
 import analytics from '@react-native-firebase/analytics';
 import TextTicker from 'react-native-text-ticker';
 
-import {API} from '@env';
+import {API, NEW_API} from '@env';
 import {windowWidth, windowHeight} from '../common';
 
 const perm = async () => {
@@ -88,12 +88,14 @@ const Playlist = ({navigation, route}) => {
 
   const fetchData = async () => {
     try {
-      let api = `${API}/redirect?id=${URlID}`;
+      let api = `${NEW_API}/redirect?id=${URlID}`;
       const response = await fetch(api, {
         method: 'GET',
+        headers: {},
       });
-
-      if (response.status === 200) {
+      // const text = await response.text();
+      // console.log('Error', response.status);
+      if (response) {
         response
           .json()
           .then((res) => {
@@ -149,12 +151,12 @@ const Playlist = ({navigation, route}) => {
 
   useEffect(() => {
     setLoading(true);
-
+    // console.log(NEW_API);
     fetchData();
   }, [isFocused]);
 
   const downloader = async (single) => {
-    const api = `${API}/download?`;
+    const api = `${NEW_API}/download?`;
     const req = perm();
     const fileStatus = await checkExists(single);
     if (req) {
@@ -162,154 +164,157 @@ const Playlist = ({navigation, route}) => {
         setVisible(true);
         setCurentDownloading(single.id);
         let artistsString = single.artist.map((item) => item.name).join();
-        let passedQuery = single.title + ' ' + single.album + ' ' + artistsString;
+        let passedQuery =
+          single.title + ' ' + single.album + ' ' + artistsString;
         const params = {
-          title : single.title,
-          album : single.album,
-          artistsString
-        }
+          title: single.title,
+          album: single.album,
+          artistsString,
+        };
         const url = new URL(api);
         let query = Object.keys(params)
-             .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-             .join('&');
+          .map(
+            (k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]),
+          )
+          .join('&');
         // url.search = new URLSearchParams(params).toString();
         //console.log(api+query);
-        const response = await fetch(api+query);
+        const response = await fetch(api + query);
 
-        response.json()
-        .then((res) => {
-          //  console.log('link fetched ....');
-          let link = res.url;
-          let duration = res.duration;
+        response
+          .json()
+          .then((res) => {
+            //  console.log('link fetched ....');
+            let link = res.url;
+            let duration = res.duration;
 
-          let promise = new Promise((resolve, reject) => {
-            if (link) {
-              let task = RNBackgroundDownloader.download({
-                id: single.title,
-                url: `${link}`,
-                destination: `${RNBackgroundDownloader.directories.documents}/${single.title}.mp3`,
-              })
-                .begin((expectedBytes) => {
-                  // console.log(`Going to download ${expectedBytes} bytes!`);
-                  setVisible(true);
-                  setDownloadPercent(0);
+            let promise = new Promise((resolve, reject) => {
+              if (link) {
+                let task = RNBackgroundDownloader.download({
+                  id: single.title,
+                  url: `${link}`,
+                  destination: `${RNBackgroundDownloader.directories.documents}/${single.title}.mp3`,
                 })
-                .progress((percent) => {
-                  // console.log(`Downloaded: ${percent * 100}%`);
-                  setDownloadPercent(percent);
-                })
-                .done(async () => {
-                  //console.log('Download is done!');
+                  .begin((expectedBytes) => {
+                    // console.log(`Going to download ${expectedBytes} bytes!`);
+                    setVisible(true);
+                    setDownloadPercent(0);
+                  })
+                  .progress((percent) => {
+                    // console.log(`Downloaded: ${percent * 100}%`);
+                    setDownloadPercent(percent);
+                  })
+                  .done(async () => {
+                    //console.log('Download is done!');
 
-                  const path = `${RNBackgroundDownloader.directories.documents}/${single.title}.mp3`;
+                    const path = `${RNBackgroundDownloader.directories.documents}/${single.title}.mp3`;
 
-                  setTracks((prev) => {
-                    const nextState = prev.map((item) =>
-                      item.id == single.id
-                        ? {
-                            ...item,
-                            downloaded: true,
-                            path: path,
-                            duration: duration,
-                          }
-                        : item,
-                    );
-
-                    return nextState;
-                  });
-
-                  try {
-                    const newDownload = {
-                      id: single.id,
-                      title: single.title,
-                      artist: single.artist[0].name,
-                      album: single.album,
-                      artwork: single.artwork,
-                      url: path,
-                      duration: duration,
-                    };
-                    const storedValue = await AsyncStorage.getItem(
-                      `@downloads`,
-                    );
-                    const prevList = await JSON.parse(storedValue);
-
-                    if (!prevList) {
-                      const newList = [newDownload];
-                      await AsyncStorage.setItem(
-                        `@downloads`,
-                        JSON.stringify(newList),
+                    setTracks((prev) => {
+                      const nextState = prev.map((item) =>
+                        item.id == single.id
+                          ? {
+                              ...item,
+                              downloaded: true,
+                              path: path,
+                              duration: duration,
+                            }
+                          : item,
                       );
-                      //console.log(newDownload)
-                      Snackbar.show({
-                        text: 'First Track added to Downloads',
-                        duration: Snackbar.LENGTH_SHORT,
-                        backgroundColor: 'red',
-                      });
-                    } else {
-                      prevList.push(newDownload);
-                      await AsyncStorage.setItem(
+
+                      return nextState;
+                    });
+
+                    try {
+                      const newDownload = {
+                        id: single.id,
+                        title: single.title,
+                        artist: single.artist[0].name,
+                        album: single.album,
+                        artwork: single.artwork,
+                        url: path,
+                        duration: duration,
+                      };
+                      const storedValue = await AsyncStorage.getItem(
                         `@downloads`,
-                        JSON.stringify(prevList),
                       );
-                      Snackbar.show({
-                        text: 'Track added to Downloads',
-                        duration: Snackbar.LENGTH_SHORT,
-                        backgroundColor: 'red',
-                      });
+                      const prevList = await JSON.parse(storedValue);
+
+                      if (!prevList) {
+                        const newList = [newDownload];
+                        await AsyncStorage.setItem(
+                          `@downloads`,
+                          JSON.stringify(newList),
+                        );
+                        //console.log(newDownload)
+                        Snackbar.show({
+                          text: 'First Track added to Downloads',
+                          duration: Snackbar.LENGTH_SHORT,
+                          backgroundColor: 'red',
+                        });
+                      } else {
+                        prevList.push(newDownload);
+                        await AsyncStorage.setItem(
+                          `@downloads`,
+                          JSON.stringify(prevList),
+                        );
+                        Snackbar.show({
+                          text: 'Track added to Downloads',
+                          duration: Snackbar.LENGTH_SHORT,
+                          backgroundColor: 'red',
+                        });
+                      }
+                    } catch (err) {
+                      //console.log(err);
                     }
-                  } catch (err) {
-                    //console.log(err);
-                  }
 
-                  // dispatch(allActions.downloadOne(single, path));
+                    // dispatch(allActions.downloadOne(single, path));
 
-                  setVisible(false);
-                  resolve(path);
-                  setDownloadPercent(1);
-                  setCurentDownloading(null);
-                })
-                .error((error) => {
-                  console.log('Download canceled due to error: ', error);
-                  setDownloadPercent(0);
-                  setVisible(false);
-                  setCurentDownloading(null);
+                    setVisible(false);
+                    resolve(path);
+                    setDownloadPercent(1);
+                    setCurentDownloading(null);
+                  })
+                  .error((error) => {
+                    console.log('Download canceled due to error: ', error);
+                    setDownloadPercent(0);
+                    setVisible(false);
+                    setCurentDownloading(null);
 
-                  Snackbar.show({
-                    text:
-                      'Pardon!  Could not download this particular file due to Youtube policies',
-                    duration: Snackbar.LENGTH_SHORT,
-                    backgroundColor: 'red',
+                    Snackbar.show({
+                      text:
+                        'Pardon!  Could not download this particular file due to Youtube policies',
+                      duration: Snackbar.LENGTH_SHORT,
+                      backgroundColor: 'red',
+                    });
+                    resolve('Failed');
                   });
-                  resolve('Failed');
+              } else {
+                setDownloadPercent(0);
+                setVisible(false);
+                setCurentDownloading(null);
+                Snackbar.show({
+                  text: 'Server Error',
+                  duration: Snackbar.LENGTH_SHORT,
+                  backgroundColor: 'red',
                 });
-            } else {
-              setDownloadPercent(0);
-                  setVisible(false);
-                  setCurentDownloading(null);
-              Snackbar.show({
-                text: 'Server Error',
-                duration: Snackbar.LENGTH_SHORT,
-                backgroundColor: 'red',
-              });
-            }
+              }
+            });
+
+            return promise;
+          })
+          .catch((error) => {
+            console.log('Download canceled due to error: ', error);
+            setDownloadPercent(0);
+            setVisible(false);
+            setCurentDownloading(null);
+
+            Snackbar.show({
+              text: 'Something went wrong in the server',
+              duration: Snackbar.LENGTH_SHORT,
+              backgroundColor: 'red',
+            });
+            //resolve('Failed');
           });
-
-          return promise;
-        })
-        .catch((error) => {
-                  console.log('Download canceled due to error: ', error);
-                  setDownloadPercent(0);
-                  setVisible(false);
-                  setCurentDownloading(null);
-
-                  Snackbar.show({
-                    text:
-                      'Something went wrong in the server',
-                    duration: Snackbar.LENGTH_SHORT,
-                    backgroundColor: 'red',
-                  });
-                  //resolve('Failed');
-                })
       }
     } else {
       Alert.alert(
